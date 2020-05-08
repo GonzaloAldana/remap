7bd5df77230f30e35ecc3c51cb1edae8605956da
Gonzalo <gon.zalo.music97@gmail.com>
2020-05-07 21:32:51 -0500

    Lazy List Generator

--- lib/screens/DistanciaMarcadorListGenerator.dart
+++ lib/screens/DistanciaMarcadorListGenerator.dart
@@ -9,11 +9,13 @@ class DistanciaMarcadorListGenerator extends StatelessWidget {
   const DistanciaMarcadorListGenerator(
       {Key key,
       @required this.listaDistanciaMarcador,
-      this.mostrarHorario = false})
+      this.mostrarHorario = false,
+      this.controller})
       : super(key: key);
 
   final List<DistanciaMarcador> listaDistanciaMarcador;
   final bool mostrarHorario;
+  final ScrollController controller;
 
   @override
   Widget build(BuildContext context) {
@@ -39,14 +41,19 @@ class DistanciaMarcadorListGenerator extends StatelessWidget {
           )
         };
 
-    return ListView(
-        children: (listaDistanciaMarcador).map((marc) {
-      return ImageCard(
-        onPressed: marc.distancia != '--' ? () => callback(marc) : null,
-        nombre: marc.marcador.nombre,
-        url: marc.marcador.imagen,
-        distancia: marc.distancia,
-      );
-    }).toList());
+    return ListView.builder(
+      controller: controller,
+      shrinkWrap: true,
+      itemCount: listaDistanciaMarcador.length,
+      itemBuilder: (context, index) {
+        DistanciaMarcador marc = listaDistanciaMarcador[index];
+        return ImageCard(
+          onPressed: marc.distancia != '--' ? () => callback(marc) : null,
+          nombre: marc.marcador.nombre,
+          url: marc.marcador.imagen,
+          distancia: marc.distancia,
+        );
+      },
+    );
   }
 }
